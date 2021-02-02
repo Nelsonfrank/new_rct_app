3a0416d7d05dea1dc4c05230bae7d7acd0958c94
nelsonfrank <nelsonfr78@outlook.com>
2021-02-02 21:56:01 +0300

    feat: Fetch data for marketplace from an api

--- src/app/users/components/market-info/MarketInfo.tsx
+++ src/app/users/components/market-info/MarketInfo.tsx
@@ -1,8 +1,10 @@
 // dependencies
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
 import { RouteComponentProps } from '@reach/router';
-
+import { GetPriceRate } from '../../../../API';
+import moment from 'moment';
+import Notification from '../../../components/notification';
 const dataSource = [
   {
     key: '1',
@@ -20,28 +22,89 @@ const dataSource = [
 
 const columns = [
   {
-    title: 'S/N',
-    dataIndex: 'serial',
-    key: 'serial',
-  },
-  {
     title: 'Variety',
     dataIndex: 'variety',
     key: 'variety',
+    sorter: {
+      // eslint-disable-next-line
+      compare: (a: any, b: any) => a.variety.length - b.variety.length,
+    },
   },
   {
-    title: 'Price(TZS/kgs)',
+    title: 'Price (TZS)',
     dataIndex: 'price',
     key: 'price',
+    sorter: {
+      // eslint-disable-next-line
+      compare: (a: any, b: any) => a.price.length - b.price.length,
+    },
+  },
+  {
+    title: 'Region',
+    dataIndex: 'region',
+    key: 'region',
+    sorter: {
+      // eslint-disable-next-line
+      compare: (a: any, b: any) => a.region.length - b.region.length,
+    },
+  },
+  {
+    title: 'Effect Date',
+    dataIndex: 'effectiveDate',
+    key: 'effectiveDate',
+    sorter: {
+      // eslint-disable-next-line
+      compare: (a: any, b: any) =>
+        a.effectiveDate.length - b.effectiveDate.length,
+    },
   },
 ];
-
 // export interface MarketInfoProps {}
 
+type priceRateProps = {
+  key: string;
+  variety: string;
+  price: string;
+  region: string;
+  effectiveDate: string;
+}[];
+
 const MarketInfo: React.FC<RouteComponentProps> = () => {
+  const [priceRate, setPriceRate] = useState<priceRateProps>([]);
+
+  useEffect(() => {
+    const getPriceRate = async () => {
+      const priceRateResponse = await GetPriceRate().then(
+        (response) => response,
+      );
+
+      if (priceRateResponse.status === 200) {
+        const data = priceRateResponse.data.data.map((item: any) => {
+          return {
+            key: item.id,
+            variety: item.variety,
+            price: item.price_rate,
+            region: item.region,
+            effectiveDate: moment(item.date).format('Do MMMM YYYY'),
+          };
+        });
+        setPriceRate(data);
+        // console.log(data);
+      } else {
+        Notification(
+          false,
+          'Fail to fetch Price Rate',
+          priceRateResponse.message,
+        );
+      }
+    };
+    getPriceRate();
+  }, []);
   return (
     <>
-      <Table dataSource={dataSource} columns={columns} />
+      <div style={{ height: '700px' }}>
+        <Table dataSource={priceRate} columns={columns} />
+      </div>
     </>
   );
 };
